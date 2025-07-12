9d90e329c03404af399e7eb743cc91587f35df91
SebGazeau <47519901+SebGazeau@users.noreply.github.com>
2025-07-12 15:36:38 +0200

    delete right column

--- src/components/shared/main-navigation.tsx
+++ src/components/shared/main-navigation.tsx
@@ -133,25 +133,10 @@ export function MainNavigation({ children }: { children: React.ReactNode }) {
     ];
 
     return (
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-            <div className="hidden border-r bg-muted/40 md:block">
-                <div className="flex h-full max-h-screen flex-col gap-2">
-                    <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                        <Link
-                            href="/"
-                            className="flex items-center gap-2 font-semibold"
-                        >
-                            <Landmark className="h-6 w-6" />
-                            <span>The Ballot Project</span>
-                        </Link>
-                    </div>
-                    
-
-                </div>
-            </div>
+        <div className="grid min-h-screen w-full">
             <div className="flex flex-col">
                         <Header pages={pages} pathname={pathname}/>
-                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 w-full max-w-screen-md place-self-center">
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 w-full place-self-center">
                     {children}
                 </main>
             </div>
